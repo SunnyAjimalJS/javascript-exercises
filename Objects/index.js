9007dd729b831e1d3e45cd05aafb4ffe3b1cb2f2
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-04-08 12:37:14 +0100

    Worked on object exercises

---
+++ Objects/index.js
@@ -0,0 +1,9 @@
+// Copy the values of all enumerable own properties from one or more source objects to a target object using Object.assign():
+const array = ["Allen", 24];
+const object = Object.assign({}, array);
+// console.log(object); // Output: { '0': 'Allen', '1': 24 }
+
+// Add items to an object from an array using the spread operator:
+const arrayTwo = ["Allen", 24];
+const objectTwo = { ...array };
+// console.log(objectTwo); // Output: { '0': 'Allen', '1': 24 }
