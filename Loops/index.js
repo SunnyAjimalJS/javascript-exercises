7545d71607258038fc37d86df10c01d826e1e81e
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-04-08 10:54:05 +0100

    Worked on Loop exercises

--- Loops/index.js
+++ Loops/index.js
@@ -32,3 +32,18 @@ for (let val of arrFour) {
   newArrThree.push(Math.sqrt(val));
 }
 // console.log(newArrThree);
+
+// Using an iterator to return a new array of values:
+let arrFive = [4, 9, 16, 25];
+let newArrFour = [];
+const it = arrFive.values();
+let item;
+while (!(item = it.next()).done) {
+  newArrFour.push(Math.sqrt(item.value));
+}
+// console.log(newArrFour);
+
+// Using a map function to return a new array of values:
+let arrSix = [4, 9, 16, 25];
+let newArrFive = arrSix.map(Math.sqrt);
+// console.log(newArrFive);
