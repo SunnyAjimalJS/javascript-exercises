d5421fcc1595938af47c3dadcf4e65bd0cf7b76f
SunnyAjimalJS <sukvinder.ajimal@live.co.uk>
2021-04-25 16:51:10 +0100

    Worked on string method exercises

---
+++ Functions/stringMethods.js
@@ -0,0 +1,9 @@
+const string = "JavaScript";
+
+// 1. Slice:
+let slicedString = string.slice(3, 5);
+// console.log(slicedString); // Output: aS
+
+// 2. Replace:
+let replacedString = string.replace("JavaScript", "JAVASCRIPT");
+console.log(replacedString); // Output: JAVASCRIPT
